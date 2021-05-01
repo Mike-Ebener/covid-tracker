44fb54de880167f51ca0d18c1f6b75ba76a39d3e
Code Wolf <brgerner@gmail.com>
2021-04-30 21:15:17 -0700

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,24 @@
+const news1 = document.getElementById('news1');
+const news2 = document.getElementById('news2');
+const news3 = document.getElementById('news3');
+const news4 = document.getElementById('news4');
+
+var newsArray = [news1, news2, news3, news4];
+
 const apiKey = "be3c3658dbb343423bac4e9c46b209e9";
 
 function getInfo(country) {
     const newsUrl = "http://api.mediastack.com/v1/news" +
         "?access_key=" + apiKey +
         "&countries=" + country +
-        "&limit=5&languages=en,-ar,-zh,-de,-it,-ru,-nl,-es,-fr,-no,-pt";
+        "&limit=4&languages=en,-ar,-zh,-de,-it,-ru,-nl,-es,-fr,-no,-pt";
     fetch(newsUrl).then(function (response) {
         if (response.ok) {
             response.json().then(function (newsData) {
                 console.log(newsData);
-                // clear html elements where news data will appear
+                
                 for (let i = 0; i < newsData.length; i++) {
-                    // create html elements to be appended
+                    newsArray
                     // add classes to newly created html elements
                     // add attributes to newly created html elements ()
                 }
